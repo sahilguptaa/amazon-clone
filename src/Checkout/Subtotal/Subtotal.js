dd1624ca9a5613714f18b85d913fa9afa36dd47f
Sahil Gupta <gupta.sahil7696@gmail.com>
2020-10-17 18:50:31 +0000

    change to reduce function

--- src/Checkout/Subtotal/Subtotal.js
+++ src/Checkout/Subtotal/Subtotal.js
@@ -3,13 +3,16 @@ import "./Subtotal.css";
 import CurrencyFormat from "react-currency-format";
 
 const Subtotal = ({ basket = [] }) => {
-  const getBasketTotal = () => {
-    let sum = 0;
-    basket.forEach((item) => {
-      sum += item.price;
-    });
-    return sum;
-  };
+  // const getBasketTotal = () => {
+  //   let sum = 0;
+  //   basket.forEach((item) => {
+  //     sum += item.price;
+  //   });
+  //   return sum;
+  // };
+
+  const getBasketTotal = () =>
+    basket?.reduce((amount, item) => item.price + amount, 0);
 
   return (
     <div className="subtotal">
@@ -18,7 +21,7 @@ const Subtotal = ({ basket = [] }) => {
           <>
             <p>
               Subtotal ({basket.length} items):
-              <strong>{`${value}`}</strong>
+              <strong>{value}</strong>
             </p>
             <small className="subtotal__gift">
               <input className="subtotal__input" type="checkbox" /> This order
